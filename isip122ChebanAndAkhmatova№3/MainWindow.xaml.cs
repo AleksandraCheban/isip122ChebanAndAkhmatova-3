741296ad9bf8d476ee6b3edfef2dffce96c15e8b
Aleksandra <blueberry04p@gmail.com>
2025-02-18 11:11:14 +0300

    Доделать исключения

--- isip122ChebanAndAkhmatova№3/MainWindow.xaml.cs
+++ isip122ChebanAndAkhmatova№3/MainWindow.xaml.cs
@@ -24,5 +24,57 @@ namespace isip122ChebanAndAkhmatova_3
         {
             InitializeComponent();
         }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            string height = textBoxHeight.Text.Trim();
+            string weight = textBoxweight.Text.Trim();
+            double answer = 0, x = Convert.ToInt32(height), y = Convert.ToDouble(weight); ;
+            if (x < 130 || x > 220 || y < 40 || y > 170)
+            {
+                MessageBox.Show("Вы ввели недопустимые значения");
+            }
+            else
+            {
+                if (RB_man.IsChecked == true)
+                {
+                    answer = x - 100 + ((x - 100) * 13 / 100);
+
+                    if (y >= (answer - 3.00) && y <= (answer + 3.00))
+                    {
+                        MessageBox.Show("С вашим весом все хорошо!");
+                    }
+                    else if (y < (answer - 3.00))
+                    {
+                        MessageBox.Show("Ваш вес ниже нормы");
+                    }
+                    else if (y > (answer + 3.00))
+                    {
+                        MessageBox.Show("Ваш вес выше нормы");
+                    };
+
+                }
+                else if (RB_woman.IsChecked == true)
+                {
+                    answer = x - 100 - ((x - 100) * 10 / 100);
+                    if (y >= (answer - 3.00) && y <= (answer + 3.00))
+                    {
+                        MessageBox.Show("С вашим весом все хорошо!");
+                    }
+                    else if (y < (answer - 3))
+                    {
+                        MessageBox.Show("Ваш вес ниже нормы");
+                    }
+                    else if (y > (answer + 3))
+                    {
+                        MessageBox.Show("Ваш вес выше нормы");
+                    };
+                }
+
+
+
+            }
+
+        }
     }
 }
